66f2ae0e1f3f48aed6af4e225c4c73d77809b264
Titvt <tqydyqt@126.com>
2019-11-14 23:50:08 +0800

    version 2.0

--- CQRobot/Robot.h
+++ CQRobot/Robot.h
@@ -6,14 +6,14 @@
 
 using namespace std;
 
-const int64_t OWNER = 1044805408LL;
-
-int32_t sendTo(int32_t ac, int64_t qq, string message) {
-	return CQ_sendPrivateMsg(ac, qq, message.c_str());
-}
+const int64_t OWNER = 1044805408LL, SELF = 3340741722LL;
 
 int32_t send(int32_t ac, int64_t group, string message) {
 	return CQ_sendGroupMsg(ac, group, message.c_str());
+}
+
+int32_t sendTo(int32_t ac, int64_t qq, string message) {
+	return CQ_sendPrivateMsg(ac, qq, message.c_str());
 }
 
 string renameReply(string reply) {
@@ -23,9 +23,36 @@ string renameReply(string reply) {
 	return reply;
 }
 
+string removeAt(string str) {
+	size_t start = 0, end;
+	while ((start = str.find("[CQ:at,qq=")) != string::npos) {
+		end = str.find("]", start);
+		str.replace(start, end - start, "");
+	}
+	return str;
+}
+
 class Robot {
 	int64_t bindedGroup;
+	vector<int64_t> chatter;
 	int32_t ac;
+
+	bool isChatter(int64_t qq) {
+		for (auto i = chatter.begin(); i < chatter.end(); i++)
+			if ((*i) == qq)
+				return true;
+		return false;
+	}
+
+	void addChatter(int64_t qq) {
+		chatter.push_back(qq);
+	}
+
+	void deleteChatter(int64_t qq) {
+		for (auto i = chatter.begin(); i < chatter.end(); i++)
+			if ((*i) == qq)
+				chatter.erase(i);
+	}
 
 public:
 	Robot(int64_t group, int32_t ac) {
@@ -38,8 +65,20 @@ public:
 	}
 
 	void processMessage(int64_t qq, string message) {
+		if (qq == OWNER) {
+			if (message.substr(0, 22) == "强制开始聊天[CQ:at,qq=") {
+				int64_t q = stoll(message.substr(22, message.length() - 24));
+				if (!isChatter(q))
+					addChatter(q);
+			}
+			if (message.substr(0, 22) == "强制结束聊天[CQ:at,qq=") {
+				int64_t q = stoll(message.substr(22, message.length() - 24));
+				if (isChatter(q))
+					deleteChatter(q);
+			}
+		}
 		if (message == "麦萌萌") {
-			send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "]\n叫我干啥(⊙o⊙)？");
+			send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "]\n你好呀~\(^0^)/\n我是麦萌萌小管家(￣▽￣～) ！\n咱在群里有以下功能哟~：\n教你百度：xxx\n百度：xxx\n翻译：xxx\n想和我单独聊天的话可以私聊我哦~\n(私聊时上述功能也可用>_<!)\n想在群里和我聊天的话有如下方式：\n开始聊天/结束聊天\n[CQ:at,qq=3340741722] xxx\n咱们愉快相处吧(●'◡'●)！");
 			return;
 		}
 		if (message.substr(0, 10) == "教你百度：") {
@@ -59,19 +98,55 @@ public:
 			return;
 		}
 		if (message.substr(0, 6) == "翻译：") {
-			send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "] 翻译结果为：\n" + XLAT(message.substr(6)));
+			string str = XLAT(message.substr(6));
+			if (str == "")
+				send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "] emmm...");
+			else
+				send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "] 翻译结果为：\n" + str);
 			return;
 		}
 		if (message.substr(0, 5) == "翻译:") {
-			send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "] 翻译结果为：\n" + XLAT(message.substr(5)));
+			string str = XLAT(message.substr(5));
+			if (str == "")
+				send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "] emmm...");
+			else
+				send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "] 翻译结果为：\n" + str);
 			return;
 		}
-		if (message.substr(0, 22) == "[CQ:at,qq=3340741722] " && message.substr(22).length() != 0) {
-			send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "]\n" + renameReply(AI(message.substr(22))));
-			return;
+		if (message.find("[CQ:at,qq=" + to_string(SELF) + "]") != string::npos) {
+			string str = removeAt(message);
+			if (str == "")
+				send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "]\n叫我干啥(⊙o⊙)？");
+			else
+				send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "]\n" + renameReply(AI(str)));
 		}
-		if (message.find("[CQ:at,qq=3340741722]") != string::npos)
-			send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "]\n叫我干啥(⊙o⊙)？");
+		if (message == "开始聊天") {
+			if (isChatter(qq)) {
+				send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "]\n咱们已经在聊天啦~！");
+				return;
+			}
+			else {
+				addChatter(qq);
+				send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "]\n开始愉快的聊天吧~！");
+				return;
+			}
+		}
+		if (message == "结束聊天") {
+			if (!isChatter(qq))
+				return;
+			else {
+				deleteChatter(qq);
+				send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "]\n下次再聊吧~！");
+				return;
+			}
+		}
+		if (isChatter(qq)) {
+			string str = removeAt(message);
+			if (str == "")
+				send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "]\n叫我干啥(⊙o⊙)？");
+			else
+				send(ac, bindedGroup, "[CQ:at,qq=" + to_string(qq) + "]\n" + renameReply(AI(str)));
+		}
 	}
 };
 
@@ -127,13 +202,23 @@ public:
 					sendTo(ac, OWNER, "已经关闭");
 				return true;
 			}
+			if (message == "查询列表") {
+				string str = "已经开启的群如下：";
+				for (auto i = robots.begin(); i < robots.end(); i++)
+					str += "\n" + to_string((*i)->getBindedGroup());
+				if (str == "已经开启的群如下：")
+					sendTo(ac, OWNER, "目前没有开启的群");
+				else
+					sendTo(ac, OWNER, str);
+				return true;
+			}
 		}
 		return false;
 	}
 
 	void processPrivateMessage(int64_t qq, string message) {
 		if (message == "麦萌萌" || message == "麦萌萌 ") {
-			sendTo(ac, qq, "你好！我是麦萌萌小管家(*^_^*)");
+			sendTo(ac, qq, "你好呀~\(^0^)/\n我是麦萌萌小管家(￣▽￣～) ！\n咱在群里有以下功能哟~：\n教你百度：xxx\n百度：xxx\n翻译：xxx\n想和我单独聊天的话可以私聊我哦~\n(私聊时上述功能也可用>_<!)\n想在群里和我聊天的话有如下方式：\n开始聊天/结束聊天\n[CQ:at,qq=3340741722] xxx\n咱们愉快相处吧(●'◡'●)！");
 			return;
 		}
 		if (message.substr(0, 10) == "教你百度：") {
@@ -153,14 +238,26 @@ public:
 			return;
 		}
 		if (message.substr(0, 6) == "翻译：") {
-			sendTo(ac, qq, "翻译结果为：\n" + XLAT(message.substr(6)));
+			string str = XLAT(message.substr(6));
+			if (str == "")
+				sendTo(ac, qq, "emmm...");
+			else
+				sendTo(ac, qq, "翻译结果为：\n" + str);
 			return;
 		}
 		if (message.substr(0, 5) == "翻译:") {
-			sendTo(ac, qq, "翻译结果为：\n" + XLAT(message.substr(5)));
+			string str = XLAT(message.substr(5));
+			if (str == "")
+				sendTo(ac, qq, "emmm...");
+			else
+				sendTo(ac, qq, "翻译结果为：\n" + str);
 			return;
 		}
-		sendTo(ac, qq, renameReply(AI(message)));
+		string str = renameReply(AI(message));
+		if (str == "")
+			sendTo(ac, qq, "emmm...");
+		else
+			sendTo(ac, qq, str);
 	}
 
 	bool preProcessGroupMessage(int64_t group, int64_t qq, string message) {
@@ -168,7 +265,7 @@ public:
 			if (message == "开启小管家") {
 				if (!isBinded(group)) {
 					addRobot(group);
-					send(ac, group, "麦萌萌小管家上线啦~！\n有事请呼唤我哟~(*^_^*)");
+					send(ac, group, "麦萌萌小管家上线啦~！\n有事请呼唤我哟~(*^_^*)\n(需要帮助的话请发送\"麦萌萌\"~(￣▽￣*)~)");
 				}
 				else
 					send(ac, group, "我在呢！/(ㄒoㄒ)/~~");
